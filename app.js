9efa64cc01c2c311fcc962f135b333ad33d369bb
Fernandes13 <Fernandes_.10@hotmail.com>
2019-05-07 00:09:15 +0100

    added color (chalk)

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-const validator = require('validator');
+const chalk = require('chalk');
 const notes = require('./notes.js');
+
+const warning = chalk.keyword('orange');
 
 console.log(notes.getNotes());
 
-console.log(validator.isURL("https/mead.io"));
+console.log(chalk.bgBlue.black('Success!'));
+console.log(warning('Ai mãe!'));
